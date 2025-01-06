63d541930516a03bfcd307e0e2851c7b97557f51
Jeremy Bejarano <jmbejara@gmail.com>
2025-01-06 17:06:40 -0600

    Remove solution

--- src/port_opt.py
+++ src/port_opt.py
@@ -2,8 +2,6 @@ import scipy.optimize
 import numpy as np
 
 def find_global_minimum_variance_portfolio(Sigma):
-    f = lambda w: w.transpose() @ Sigma @ w
-    con = lambda w: np.sum(w) - 1
-    w0 = np.array([.2, .2, .2, .2, .2])
-    wstar = scipy.optimize.fmin_slsqp(f, w0, f_eqcons=con, acc=1e-09, iprint=2)
+    ## TODO: Implement the global minimum variance portfolio
+    wstar = None
     return wstar
