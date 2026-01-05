f53949923e3fad864cbbb4cb8396f4bbde6fa8d8
Jeremy Bejarano <jmbejara@gmail.com>
2026-01-05 08:53:45 -0600

    Remove solution

--- src/port_opt.py
+++ src/port_opt.py
@@ -2,8 +2,10 @@ import scipy.optimize
 import numpy as np
 
 def find_global_minimum_variance_portfolio(Sigma):
-    w0 = np.array([.2, .2, .2, .2, .2])
-    f = lambda w: w.T @ Sigma @ w
-    con = lambda w: np.sum(w) - 1
-    wstar = scipy.optimize.fmin_slsqp(f, w0, f_eqcons=con, acc=1e-09, iprint=2)
+    ## TODO: YOUR CODE HERE
+    # w0 = np.array([.2, .2, .2, .2, .2])
+    # f = lambda w: w.T @ Sigma @ w
+    # con = lambda w: np.sum(w) - 1
+    # wstar = scipy.optimize.fmin_slsqp(f, w0, f_eqcons=con, acc=1e-09, iprint=2)
+    wstar = np.array([.2, .2, .2, .2, .2])
     return wstar
